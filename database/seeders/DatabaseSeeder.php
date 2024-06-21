cec2b0d8d48463bd16e7b81356abb0135d93c669
alifndaru <alifndaru8@gmail.com>
2024-06-22 00:37:20 +0700

    feat: Add TerminalSeeder to populate terminal data in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Terminal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersSeeder::class,
+            TerminalSeeder::class,
         ]);
     }
 }
